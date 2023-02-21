1adbe0995550f6f9c8d96cfb41e0a52026cb7e55
79650 <last137954682@gmail.ru>
2023-02-21 03:46:08 +0300

    Coursework2

--- src/Task2/Task2.java
+++ src/Task2/Task2.java
@@ -11,21 +11,21 @@ public class Task2 {
     public static void main(String[] args) {
 
         String input = "yourapp the quick yourapp brown fox jumps yourapp over the lazy yourapp dog";
-        List<String> list = new ArrayList<>(Collections.singleton("yourapp the quick yourapp brown fox jumps yourapp over the lazy yourapp dog"));
-        list.stream()
-                        .flatMap(line->Stream.of(input.split(" ")))
-                                .collect(Collectors.toMap(key->key,val->1,Integer::sum))
-                                                .entrySet().stream()
-                        .sorted((e1,e2)-> {
+        List<String>list = new ArrayList<>(Collections.singleton("yourapp the quick yourapp brown fox jumps yourapp over the lazy yourapp dog"));
+                list.stream()
+                        .flatMap(line -> Stream.of(input.split(" ")))
+                        .collect(Collectors.toMap(key -> key, val -> 1, Integer::sum))
+                        .entrySet().stream()
+                        .sorted((e1, e2) -> {
                             int val = e1.getValue().compareTo(e2.getValue()) * -1;
                             if (val == 0) {
                                 val = e1.getKey().compareTo(e2.getKey());
                             }
                             return val;
                         })
-                .forEach(e-> System.out.println(e.getKey()+ " " +e.getValue()));
+                        .forEach(e -> System.out.println(e.getKey() + " " + e.getValue()));
 
-    }
+            }
 
+        }
 
-}
